3308118a3f18c39bbe9d3b64395672d18fff5d6f
Willem van den Ende <willem@qwan.eu>
2015-04-14 08:28:02 +0100

    Adding smartparens

--- init.el
+++ init.el
@@ -4,6 +4,19 @@
 
 (add-to-list 'load-path "~/dev/spikes/haskell/my-emacs-mode/")
 
+(package-initialize)
+(if (not (package-installed-p 'use-package))
+    (progn
+      (package-refresh-contents)
+      (package-install 'use-package)))
+
+(eval-when-compile
+  (require 'use-package))
+
 (load-file "cm-haskell.el")
 
+(add-to-list 'load-path "./yaml-mode")
+(require 'yaml-mode) (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
+(use-package smartparens
+  :init (smartparens-global-mode t))
